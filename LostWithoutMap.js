ba2a0206f5204dadf8be685fdec5538a41d9931a
Akshat Vishwakarma <akvs0707@gmail.com>
2022-10-03 22:24:11 +0530

    add: Lost without a map solution

---
+++ LostWithoutMap.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+
+function maps(x) {
+  console.log(x.map((val) => val * 2));
+}
+
+maps([1, 2, 3]);
+maps([4, 1, 1, 1, 4]);
+maps([2, 2, 2, 2, 2, 2]);
